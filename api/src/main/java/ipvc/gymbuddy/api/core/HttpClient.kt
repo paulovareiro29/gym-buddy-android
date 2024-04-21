f12ae6d1e220132fb4b898c28d4d654947d3e428
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-04-21 12:12:25 +0100

    feat: added buildconfig variables

--- api/src/main/java/ipvc/gymbuddy/api/core/HttpClient.kt
+++ api/src/main/java/ipvc/gymbuddy/api/core/HttpClient.kt
@@ -1,11 +1,12 @@
 package ipvc.gymbuddy.api.core
 
+import ipvc.gymbuddy.api.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 open class HttpClient<T>(service: Class<T>) {
     companion object {
-        private const val BASE_URL = "http://192.168.78.16:3000/api/"
+        private const val BASE_URL = BuildConfig.API_URL + "/api/"
     }
 
     protected var client: T = Retrofit.Builder()
