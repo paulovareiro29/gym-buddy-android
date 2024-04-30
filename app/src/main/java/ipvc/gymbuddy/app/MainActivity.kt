a61e3278be301a9b58614516badacbbcd364974b
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-04-30 21:29:33 +0100

    feat: added navigation

--- app/src/main/java/ipvc/gymbuddy/app/MainActivity.kt
+++ app/src/main/java/ipvc/gymbuddy/app/MainActivity.kt
@@ -2,16 +2,19 @@ package ipvc.gymbuddy.app
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import ipvc.gymbuddy.app.viewmodels.AuthenticationViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var authenticationViewModel: AuthenticationViewModel
+    private lateinit var navController: NavController
+    // private lateinit var authenticationViewModel: AuthenticationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +27,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        authenticationViewModel = ViewModelProvider(this)[AuthenticationViewModel::class.java]
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        // setupActionBarWithNavController(navController)
+
+        // authenticationViewModel = ViewModelProvider(this)[AuthenticationViewModel::class.java]
         /* authenticationViewModel.user.observe(this) {
             findViewById<TextView>(R.id.text).text = it?.email ?: ""
         } */
     }
 
-    fun handleButtonClick(v: View?) {
-        authenticationViewModel.login("gymbuddy@ipvc.pt", "gymbuddy")
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    /* fun handleButtonClick(v: View?) {
+        authenticationViewModel.login("gymbuddy@ipvc.pt", "gymbuddy")
+    } */
 }
 
