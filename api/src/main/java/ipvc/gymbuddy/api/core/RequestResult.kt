05a576cd71e58fb54fa5a75c8ceef7ad23a6bd96
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-05-06 22:43:52 +0100

    style: spacing

--- api/src/main/java/ipvc/gymbuddy/api/core/RequestResult.kt
+++ api/src/main/java/ipvc/gymbuddy/api/core/RequestResult.kt
@@ -1,4 +1,5 @@
 package ipvc.gymbuddy.api.core
+
 sealed class RequestResult<out T> {
     data class Success<out T>(val code: Int, val message: String, val data: T) : RequestResult<T>()
     data class Error(val code: Int, val message: String, val errors: Map<String, String>) : RequestResult<Nothing>()
