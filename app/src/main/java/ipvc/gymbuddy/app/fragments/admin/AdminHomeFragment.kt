89febde12cf944c729ca34efe692d03c90a7ff60
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-05-09 22:51:02 +0100

    feat: admin home page user info

--- app/src/main/java/ipvc/gymbuddy/app/fragments/admin/AdminHomeFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/admin/AdminHomeFragment.kt
@@ -1,9 +1,11 @@
 package ipvc.gymbuddy.app.fragments.admin
 
 import android.os.Bundle
+import android.view.View
 import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentAdminHomeBinding
 import ipvc.gymbuddy.app.viewmodels.AuthenticationViewModel
+import java.util.Locale
 
 class AdminHomeFragment : BaseFragment<FragmentAdminHomeBinding>(
     FragmentAdminHomeBinding::inflate
@@ -12,5 +14,11 @@ class AdminHomeFragment : BaseFragment<FragmentAdminHomeBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = getViewModel()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.name.text = viewModel.user.value!!.name
+        binding.role.text = viewModel.user.value!!.role.name.replaceFirstChar { it.titlecase(Locale.ROOT) }
     }
 }
