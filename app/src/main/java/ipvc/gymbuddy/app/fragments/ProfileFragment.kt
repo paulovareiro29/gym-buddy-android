570570233c597a7896ad7bcc36f685d9d3e60e0a
Diogo Bernardes <Diogobernardes@ipvc.pt>
2024-05-11 22:53:09 +0100

    feat: profile screen

--- app/src/main/java/ipvc/gymbuddy/app/fragments/ProfileFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package ipvc.gymbuddy.app.fragments
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentProfileBinding
 import ipvc.gymbuddy.app.viewmodels.AuthenticationViewModel
@@ -12,5 +14,20 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = getViewModel()
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.user.observe(viewLifecycleOwner, Observer  { user ->
+            user?.let {
+                binding.apply {
+                    name.text = user.name?.toString() ?: ""
+                    firstName.setText(user.name?.toString() ?: "")
+                    gender.setText(user.email?.toString() ?: "")
+                    address.setText(user.address?.toString() ?: "")
+                    role.text = user.role?.name?.toString() ?: ""
+                }
+            }
+        })
     }
 }
