56a05b2d5ad3b702c1f207f8aadffdaceab8d0b6
Oigres2 <sergiovb2002@hotmail.com>
2024-05-25 21:24:55 +0100

    feat: Change language on app

--- app/src/main/java/ipvc/gymbuddy/app/fragments/SettingsFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/SettingsFragment.kt
@@ -1,8 +1,47 @@
 package ipvc.gymbuddy.app.fragments
 
+import android.os.Bundle
+import android.view.View
+import ipvc.gymbuddy.app.R
+import android.content.res.Configuration
+import java.util.Locale
+import androidx.appcompat.app.AlertDialog
 import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentSettingsBinding
 
-class SettingsFragment : BaseFragment<FragmentSettingsBinding>(
-    FragmentSettingsBinding::inflate
-)
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.changeLanguage.setOnClickListener {
+            showLanguageDialog()
+        }
+    }
+
+    private fun showLanguageDialog() {
+        val languages = arrayOf("English", "Português")
+        val languageCodes = arrayOf("en", "pt")
+
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.change_language))
+            .setItems(languages) { dialog, which ->
+                changeLanguage(languageCodes[which])
+                updateViews()
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun changeLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        requireContext().resources.updateConfiguration(config, requireContext().resources.displayMetrics)
+    }
+
+    private fun updateViews() {
+        binding.changeLanguage.text = getString(R.string.change_language)
+        binding.logout.text = getString(R.string.logout)
+    }
+}
