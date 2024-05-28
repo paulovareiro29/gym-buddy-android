d7b31d2059312c4d5f8ab2450a81d8b4525e0893
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-05-28 11:59:10 +0100

    fix: change dependency location

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.security.crypto)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
