ffa001727a92ad7e385d80bf7325f40855a875c1
Sérgio Barbosa <106695093+Oigres2@users.noreply.github.com>
2024-05-31 14:36:39 +0100

    feat: settings screen (#17)
    * feat: settings starter layout
    
    * feat: button icons and translation
    
    * feat: Change language on app
    
    * feat: changed change language layout
    
    * feat: review changes / added flags and animation arrow on click
    
    * fix: modern way change language
    
    * fix: rotation crash
    
    ---------
    
    Co-authored-by: Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>

--- app/src/main/java/ipvc/gymbuddy/app/fragments/SettingsFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/SettingsFragment.kt
@@ -1,8 +1,57 @@
 package ipvc.gymbuddy.app.fragments
 
+import android.os.Bundle
+import android.view.View
+import ipvc.gymbuddy.app.R
 import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentSettingsBinding
+import android.animation.ObjectAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 
-class SettingsFragment : BaseFragment<FragmentSettingsBinding>(
-    FragmentSettingsBinding::inflate
-)
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
+    private var isLanguageOptionsVisible = false
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadToolbar(getString(R.string.settings), true)
+
+        binding.changeLanguage.setOnClickListener {
+            toggleLanguageOptions()
+        }
+
+        binding.englishOption.setOnClickListener { selectLanguage("en") }
+        binding.portugueseOption.setOnClickListener { selectLanguage("pt") }
+        binding.logout.setOnClickListener { handleLogout() }
+    }
+
+    private fun toggleLanguageOptions() {
+        if (!isLanguageOptionsVisible) {
+            binding.languageOptions.visibility = View.VISIBLE
+            rotateArrow(0f, 90f)
+        } else {
+            binding.languageOptions.visibility = View.GONE
+            rotateArrow(90f, 0f)
+        }
+        isLanguageOptionsVisible = !isLanguageOptionsVisible
+    }
+
+    private fun rotateArrow(fromDegrees: Float, toDegrees: Float) {
+        val animator = ObjectAnimator.ofFloat(binding.arrowIcon, "rotation", fromDegrees, toDegrees)
+        animator.duration = 300
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.start()
+    }
+
+    private fun selectLanguage(language: String) {
+        binding.languageOptions.visibility = View.GONE
+        if (isLanguageOptionsVisible) {
+            rotateArrow(90f, 0f)
+            isLanguageOptionsVisible = false
+        }
+
+        super.changeLanguage(language)
+    }
+
+    private fun handleLogout(){
+    }
+
+}
