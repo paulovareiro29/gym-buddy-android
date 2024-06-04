2b74bd2245d7f57a4924249be00d1ed7e9d33c52
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-06-04 23:03:19 +0100

    feat: add tabs to group exercises by day

--- app/src/main/java/ipvc/gymbuddy/app/adapters/TrainingPlanExerciseAdapter.kt
+++ app/src/main/java/ipvc/gymbuddy/app/adapters/TrainingPlanExerciseAdapter.kt
@@ -1,63 +1,43 @@
 package ipvc.gymbuddy.app.adapters
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ipvc.gymbuddy.api.models.PlanExercise
 import ipvc.gymbuddy.app.R
+import ipvc.gymbuddy.app.core.BaseRecyclerAdapter
+import ipvc.gymbuddy.app.core.BaseViewHolder
 
-class TrainingPlanExerciseAdapter(private val dataset: List<PlanExercise>) :
-    RecyclerView.Adapter<TrainingPlanExerciseAdapter.ViewHolder>() {
+class TrainingPlanExerciseAdapter(dataset: List<PlanExercise>)
+    : BaseRecyclerAdapter<PlanExercise, TrainingPlanExerciseAdapter.ViewHolder>(dataset) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recycle_adapter_training_plan_exercise, parent, false)
+    class ViewHolder(view: View) : BaseViewHolder(view) {
+        val exerciseName: TextView = view.findViewById(R.id.exercise_name)
+        val machineName: TextView = view.findViewById(R.id.machine_name)
+        val setsAndReps: TextView = view.findViewById(R.id.sets_and_reps)
+        val exercisePhoto: ImageView = view.findViewById(R.id.exercise_image)
+    }
+
+    override fun getItemLayout(): Int = R.layout.recycle_adapter_training_plan_exercise
+
+    override fun createViewHolder(view: View): ViewHolder {
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val exercise = dataset[position]
-        holder.bind(exercise)
-    }
+    override fun bindViewHolder(holder: ViewHolder, item: PlanExercise) {
+        val context = holder.itemView.context
+        val setsText = context.getString(R.string.sets)
+        val repsText = context.getString(R.string.reps)
+        val setsAndRepsText = "${item.sets} $setsText - ${item.repetitions} $repsText"
 
-    override fun getItemCount(): Int = dataset.size
+        holder.exerciseName.text = item.exercise.name
+        holder.machineName.text = item.exercise.machine.name
+        holder.setsAndReps.text = setsAndRepsText
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val exerciseName: TextView = itemView.findViewById(R.id.exercise_name)
-        private val machineName: TextView = itemView.findViewById(R.id.machine_name)
-        private val setsAndReps: TextView = itemView.findViewById(R.id.sets_and_reps)
-        private val exercisePhoto: ImageView = itemView.findViewById(R.id.exercise_image)
-        private val dayTitle: TextView = itemView.findViewById(R.id.day_header)
-
-        fun bind(exercise: PlanExercise) {
-            exerciseName.text = exercise.exercise.name
-            machineName.text = exercise.exercise.machine.name
-
-            val context = itemView.context
-            val setsText = context.getString(R.string.sets)
-            val repsText = context.getString(R.string.reps)
-            val setsAndRepsText = "${exercise.sets} $setsText - ${exercise.repetitions} $repsText"
-            setsAndReps.text = setsAndRepsText
-
-            Glide.with(context)
-                .load(exercise.exercise.photo)
-                .placeholder(R.drawable.baseline_assignment_24)
-                .into(exercisePhoto)
-
-            if (adapterPosition == 0 || dataset[adapterPosition - 1].day != exercise.day) {
-                dayTitle.visibility = View.VISIBLE
-                dayTitle.text = buildString {
-                    append(context.getString(R.string.day))
-                    append(" ${exercise.day}")
-                }
-
-            } else {
-                dayTitle.visibility = View.GONE
-            }
-        }
+        Glide.with(context)
+            .load(item.exercise.photo)
+            .placeholder(R.drawable.baseline_assignment_24)
+            .into(holder.exercisePhoto)
     }
 }
--- app/src/main/java/ipvc/gymbuddy/app/fragments/trainer/trainingPlans/exercises/TrainerTrainingPlanExercisesOverviewFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/trainer/trainingPlans/exercises/TrainerTrainingPlanExercisesOverviewFragment.kt
@@ -1,65 +1,61 @@
 package ipvc.gymbuddy.app.fragments.trainer.trainingPlans.exercises
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 import ipvc.gymbuddy.api.models.TrainingPlan
 import ipvc.gymbuddy.app.R
 import ipvc.gymbuddy.app.adapters.TrainingPlanExerciseAdapter
 import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentTrainerTrainingPlanExercisesOverviewBinding
+import ipvc.gymbuddy.app.fragments.ui.TabRecyclerViewFragment
 import ipvc.gymbuddy.app.viewmodels.trainer.planExercise.TrainerTrainingPlanExerciseOverviewModel
 
 class TrainerTrainingPlanExercisesOverviewFragment : BaseFragment<FragmentTrainerTrainingPlanExercisesOverviewBinding>(
     FragmentTrainerTrainingPlanExercisesOverviewBinding::inflate) {
 
     private lateinit var viewModel: TrainerTrainingPlanExerciseOverviewModel
-    private lateinit var recyclerView: RecyclerView
     private var trainingPlan: TrainingPlan? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = getViewModel()
         arguments?.let {
-            val trainingPlanJson = it.getString("trainingPlan")
-            trainingPlan = Gson().fromJson(trainingPlanJson, TrainingPlan::class.java)
+            trainingPlan = Gson().fromJson(it.getString("trainingPlan"), TrainingPlan::class.java)
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        trainingPlan?.let { loadToolbar(it.name) }
+        if (trainingPlan == null) navController.navigateUp()
 
-        recyclerView = view.findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        Log.d("Training Plan id", trainingPlan!!.id)
+        loadToolbar(trainingPlan!!.name)
 
+        viewModel.getPlanExercises(trainingPlan!!.id)
 
-        trainingPlan?.id?.let { planId ->
-            viewModel.getPlanExercises(planId)
-        }
-
+        val viewPager: ViewPager2 = view.findViewById(R.id.view_pager)
+        val tabLayout: TabLayout = view.findViewById(R.id.tab_layout)
         viewModel.planExerciseData.observe(viewLifecycleOwner) { resource ->
-            resource.data?.let { exercises ->
-                val groupedExercises = exercises.groupBy { it.day }
-                val sortedExercises = groupedExercises.toSortedMap().values.flatten()
-                val adapter = TrainingPlanExerciseAdapter(sortedExercises)
-                recyclerView.adapter = adapter
+            val exercises = resource.data ?: return@observe
+            val uniqueDays = exercises.map { it.day }.distinct()
+
+            val fragments = uniqueDays.map { day ->
+                val dayExercises = exercises.filter { it.day == day }
+                TabRecyclerViewFragment(TrainingPlanExerciseAdapter(dayExercises))
             }
+
+            viewPager.adapter = object : FragmentStateAdapter(this) {
+                override fun getItemCount() = fragments.size
+                override fun createFragment(position: Int) = fragments[position]
+            }
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = uniqueDays[position].toString()
+            }.attach()
         }
-
-        binding.searchInput.editText?.addTextChangedListener { handleSearch(it.toString()) }
     }
-
-    private fun handleSearch(search: String) {
-        val filtered = viewModel.planExerciseData.value?.data?.filter {
-            it.exercise.name.contains(search, true)
-        } ?: listOf()
-        recyclerView.adapter = TrainingPlanExerciseAdapter(filtered)
-    }
-
 }
