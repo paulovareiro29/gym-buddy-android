50de2d40a4ac7fe3da9a4de3533f087f492cc64a
Oigres2 <sergiovb2002@hotmail.com>
2024-06-14 15:36:32 +0100

    feat: overview client training plans / client training plans page

---
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/client/ClientTrainingPlansOverviewFragment.kt
@@ -0,0 +1,42 @@
+package ipvc.gymbuddy.app.fragments.client
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import ipvc.gymbuddy.app.adapters.UserPlanAdapter
+import ipvc.gymbuddy.app.databinding.FragmentClientUserPlanOverviewBinding
+import ipvc.gymbuddy.app.viewmodels.client.TrainerClientPlanViewModel
+
+class ClientTrainingPlansOverviewFragment : Fragment() {
+
+    private var _binding: FragmentClientUserPlanOverviewBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: TrainerClientPlanViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentClientUserPlanOverviewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[TrainerClientPlanViewModel::class.java]
+
+        val userId = arguments?.getString("userId") ?: throw IllegalStateException("User ID must be passed to ClientTrainingPlansOverviewFragment")
+
+        viewModel.getUserPlans(userId)
+        viewModel.userPlans.observe(viewLifecycleOwner) { asyncData ->
+            binding.recyclerView.layoutManager = LinearLayoutManager(context)
+            binding.recyclerView.adapter = UserPlanAdapter(asyncData.data ?: emptyList())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
