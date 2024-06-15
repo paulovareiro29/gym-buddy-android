4480cf7b0f60a0fa80146bf22d3b131daf5bf34c
Oigres2 <sergiovb2002@hotmail.com>
2024-06-15 03:01:12 +0100

    feat: added toolbar

--- app/src/main/java/ipvc/gymbuddy/app/fragments/client/ClientTrainingPlansOverviewFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/client/ClientTrainingPlansOverviewFragment.kt
@@ -1,42 +1,38 @@
 package ipvc.gymbuddy.app.fragments.client
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import ipvc.gymbuddy.app.R
+import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.adapters.ClientUserPlanAdapter
 import ipvc.gymbuddy.app.databinding.FragmentClientUserPlanOverviewBinding
 import ipvc.gymbuddy.app.viewmodels.client.TrainerClientPlanViewModel
 
-class ClientTrainingPlansOverviewFragment : Fragment() {
+class ClientTrainingPlansOverviewFragment : BaseFragment<FragmentClientUserPlanOverviewBinding>(
+    FragmentClientUserPlanOverviewBinding::inflate
+) {
 
-    private var _binding: FragmentClientUserPlanOverviewBinding? = null
-    private val binding get() = _binding!!
     private lateinit var viewModel: TrainerClientPlanViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentClientUserPlanOverviewBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = getViewModel()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[TrainerClientPlanViewModel::class.java]
+        loadToolbar(getString(R.string.training_plans_overview))
 
         val userId = arguments?.getString("userId") ?: throw IllegalStateException("User ID must be passed to ClientTrainingPlansOverviewFragment")
 
         viewModel.getUserPlans(userId)
         viewModel.userPlans.observe(viewLifecycleOwner) { asyncData ->
-            binding.recyclerView.layoutManager = LinearLayoutManager(context)
-            binding.recyclerView.adapter = ClientUserPlanAdapter(asyncData.data ?: emptyList())
+            if (asyncData.data != null) {
+                binding.recyclerView.layoutManager = LinearLayoutManager(context)
+                binding.recyclerView.adapter = ClientUserPlanAdapter(asyncData.data)
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
