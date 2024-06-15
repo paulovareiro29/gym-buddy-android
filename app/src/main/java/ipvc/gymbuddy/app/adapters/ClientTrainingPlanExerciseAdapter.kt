981810c027d44f541e6ffd8d8b83cca2e4eacaaa
Oigres2 <sergiovb2002@hotmail.com>
2024-06-15 02:37:10 +0100

    feat: code fix

--- app/src/main/java/ipvc/gymbuddy/app/adapters/ClientTrainingPlanExerciseAdapter.kt
+++ app/src/main/java/ipvc/gymbuddy/app/adapters/ClientTrainingPlanExerciseAdapter.kt
@@ -3,8 +3,10 @@ package ipvc.gymbuddy.app.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ipvc.gymbuddy.api.models.PlanExercise
 import ipvc.gymbuddy.app.R
 
@@ -14,6 +16,7 @@ class ClientTrainingPlanExerciseAdapter(private val exercises: List<PlanExercise
         val exerciseName: TextView = view.findViewById(R.id.exercise_name)
         val machineName: TextView = view.findViewById(R.id.machine_name)
         val setsAndReps: TextView = view.findViewById(R.id.sets_and_reps)
+        val exerciseImage: ImageView = view.findViewById(R.id.exercise_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
@@ -23,9 +26,19 @@ class ClientTrainingPlanExerciseAdapter(private val exercises: List<PlanExercise
 
     override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
         val exercise = exercises[position]
+        val context = holder.itemView.context
+        val sets = context.getString(R.string.sets, exercise.sets)
+        val repetitions = context.getString(R.string.reps, exercise.repetitions)
+        val setsAndRepsText = "$sets - $repetitions"
+
         holder.exerciseName.text = exercise.exercise.name
         holder.machineName.text = exercise.exercise.machine.name
-        holder.setsAndReps.text = "${exercise.sets} Sets - ${exercise.repetitions} Reps"
+        holder.setsAndReps.text = setsAndRepsText
+
+        Glide.with(context)
+            .load(exercise.exercise.photo)
+            .placeholder(R.drawable.baseline_fitness_center_24)
+            .into(holder.exerciseImage)
     }
 
     override fun getItemCount() = exercises.size
