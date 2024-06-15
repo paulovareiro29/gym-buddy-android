044dc2fc02e54f75bf4fff7489d4fc9bd91106d4
Oigres2 <sergiovb2002@hotmail.com>
2024-06-15 03:44:14 +0100

    feat: adjusted client exercise layout

--- app/src/main/java/ipvc/gymbuddy/app/fragments/client/ClientTrainingPlanExercisesOverviewFragment.kt
+++ app/src/main/java/ipvc/gymbuddy/app/fragments/client/ClientTrainingPlanExercisesOverviewFragment.kt
@@ -1,51 +1,63 @@
 package ipvc.gymbuddy.app.fragments.client
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 import ipvc.gymbuddy.api.models.TrainingPlan
+import ipvc.gymbuddy.app.R
 import ipvc.gymbuddy.app.adapters.ClientTrainingPlanExerciseAdapter
-import ipvc.gymbuddy.app.core.AsyncData
+import ipvc.gymbuddy.app.core.BaseFragment
 import ipvc.gymbuddy.app.databinding.FragmentClientTrainingPlanExercisesOverviewBinding
+import ipvc.gymbuddy.app.fragments.ui.TabRecyclerViewFragment
 import ipvc.gymbuddy.app.viewmodels.trainer.planExercise.TrainerTrainingPlanExerciseOverviewModel
 
-class ClientTrainingPlanExercisesOverviewFragment : Fragment() {
-    private var _binding: FragmentClientTrainingPlanExercisesOverviewBinding? = null
-    private val binding get() = _binding!!
+class ClientTrainingPlanExercisesOverviewFragment : BaseFragment<FragmentClientTrainingPlanExercisesOverviewBinding>(
+    FragmentClientTrainingPlanExercisesOverviewBinding::inflate
+) {
 
     private lateinit var viewModel: TrainerTrainingPlanExerciseOverviewModel
     private var trainingPlan: TrainingPlan? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentClientTrainingPlanExercisesOverviewBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = getViewModel()
+        arguments?.let {
+            trainingPlan = Gson().fromJson(it.getString("trainingPlan"), TrainingPlan::class.java)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[TrainerTrainingPlanExerciseOverviewModel::class.java]
+        if (trainingPlan == null) navController.navigateUp()
 
-        val planJson = requireArguments().getString("trainingPlan")
-        trainingPlan = Gson().fromJson(planJson, TrainingPlan::class.java)
+        loadToolbar(trainingPlan!!.name)
 
         viewModel.getPlanExercises(trainingPlan!!.id)
-        viewModel.planExerciseData.observe(viewLifecycleOwner) { asyncData ->
-            if (asyncData.status == AsyncData.Status.SUCCESS) {
-                binding.recyclerView.layoutManager = LinearLayoutManager(context)
-                asyncData.data?.let {
-                    binding.recyclerView.adapter = ClientTrainingPlanExerciseAdapter(it)
-                }
-            }
-        }
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val viewPager: ViewPager2 = view.findViewById(R.id.view_pager)
+        val tabLayout: TabLayout = view.findViewById(R.id.tab_layout)
+        viewModel.planExerciseData.observe(viewLifecycleOwner) { resource ->
+            val exercises = resource.data ?: return@observe
+            val uniqueDays = exercises.map { it.day }.distinct()
+
+            val fragments = uniqueDays.map { day ->
+                val dayExercises = exercises.filter { it.day == day }
+                val adapter = ClientTrainingPlanExerciseAdapter(dayExercises)
+                TabRecyclerViewFragment(adapter)
+            }
+
+            viewPager.adapter = object : FragmentStateAdapter(this) {
+                override fun getItemCount() = fragments.size
+                override fun createFragment(position: Int) = fragments[position]
+            }
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = uniqueDays[position]
+            }.attach()
+        }
     }
 }
