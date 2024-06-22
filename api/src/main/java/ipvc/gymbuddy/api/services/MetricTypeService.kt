fa1484847009205dfce2c4f89c71d047dd9a5eb6
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2024-06-22 12:14:23 +0100

    feat: implement create metric type (#48)
    * feat: implement metric type overview
    
    * feat: implement create metric type

--- api/src/main/java/ipvc/gymbuddy/api/services/MetricTypeService.kt
+++ api/src/main/java/ipvc/gymbuddy/api/services/MetricTypeService.kt
@@ -4,6 +4,9 @@ import ipvc.gymbuddy.api.core.HttpClient
 import ipvc.gymbuddy.api.core.RequestResult
 import ipvc.gymbuddy.api.core.ResponseParser
 import ipvc.gymbuddy.api.interfaces.IMetricTypeService
+import ipvc.gymbuddy.api.models.requests.metricTypes.CreateMetricTypeRequest
+import ipvc.gymbuddy.api.models.responses.metricTypes.CreateMetricTypeResponse
+import ipvc.gymbuddy.api.models.responses.metricTypes.DeleteMetricTypeResponse
 import ipvc.gymbuddy.api.models.responses.metricTypes.GetAllMetricTypesResponse
 
 class MetricTypeService  : HttpClient<IMetricTypeService>(IMetricTypeService::class.java) {
@@ -15,6 +18,28 @@ class MetricTypeService  : HttpClient<IMetricTypeService>(IMetricTypeService::cl
                 RequestResult.Success(response.code, response.message, parsedData)
             }
 
+            is RequestResult.Error -> response
+        }
+    }
+
+    suspend fun createMetricType(body: CreateMetricTypeRequest): RequestResult<CreateMetricTypeResponse> {
+        return when (val response = request(api.createMetricType(body))) {
+            is RequestResult.Success -> RequestResult.Success(
+                code = response.code,
+                message = response.message,
+                data = ResponseParser.payload<CreateMetricTypeResponse>(response)
+            )
+            is RequestResult.Error -> response
+        }
+    }
+
+    suspend fun deleteMetricType(id: String): RequestResult<DeleteMetricTypeResponse> {
+        return when (val response = request(api.deleteMetricType(id))) {
+            is RequestResult.Success -> RequestResult.Success(
+                code = response.code,
+                message = response.message,
+                data = ResponseParser.payload<DeleteMetricTypeResponse>(response)
+            )
             is RequestResult.Error -> response
         }
     }
